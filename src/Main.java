bb9540dfca13c86721ec043976714cd1f9ed20fb
tito <jfctito@gmail.com>
2024-03-24 19:51:34 +0100

    First commit array with keywords done!

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String[] keywords = {"Pandora", "Pinterest", "Paypal", "Pg&e", "Project free tv", "Priceline", "Press democrat", "Progressive", "Project runway",
+                "Proactive", "Programming", "Progeria", "Progesterone", "Progenex", "Procurable", "Processor", "Proud", "Print", "Prank",
+                "Bowl", "Owl", "River", "Phone", "Kayak", "Stamps", "Reprobe"};
+
+        for (int i=0; i<4; i++){
+            System.out.println(keywords[i]);
+        }
+    }
+}
